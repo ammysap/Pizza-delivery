ab8953c2a4c7ddd67587a48d5fbf0e270f9924b0
Aman <amansoni875@gmail.com>
2021-12-22 18:06:35 +0530

    add cart page and auth

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
-const express =require("express");
-const app=express();
-const path =require("path");
-const ejs =require("ejs");
-const expressLayout=require("express-ejs-layouts");
-const PORT =process.env.PORT||3000
+const express = require("express");
+const app = express();
+const path = require("path");
+const ejs = require("ejs");
+const expressLayout = require("express-ejs-layouts");
+const res = require("express/lib/response");
+const PORT = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+app.use(expressLayout);
 
-app.use(express.static('public'));
-app.get("/",function(req,res)
-{
-    res.render('home');
+app.set("views", path.join(__dirname, "/resources/views"));
+
+app.set("view engine", "ejs");
+app.get("/", function (req, res) {
+  res.render("home");
+});
+app.get("/cart", function (req, res) {
+  res.render("customers/cart");
+});
+app.get("/login", function (req, res) {
+  res.render("auth/login");
+});
+app.get("/register", function (req, res) {
+  res.render("auth/register");
 });
 
 //set template engine
 
-app.use(expressLayout);
-
-
-app.set("views",path.join(__dirname,"/resources/views"));
-
-app.set("view engine", "ejs");
-
-app.listen(PORT,function()
-{
-    console.log("server started on port 3000");
+app.listen(PORT, function () {
+  console.log("server started on port 3000");
 });
