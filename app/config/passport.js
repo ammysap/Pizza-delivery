d1d044e7389859b20d102be266d57d553f1867f0
Aman <amansoni875@gmail.com>
2023-07-08 18:38:04 +0530

    npm update

--- app/config/passport.js
+++ app/config/passport.js
@@ -17,7 +17,7 @@ function init(passport) {
       password,
       done
     ) {
-      const user = await User.findOne({ email: email });
+      const user = await User.findOne({ email: email });passport-local-mongoose
       if (!user) {
         return done(null, false, { message: "no user with this email" });
       }
